c3489cc9815e5e0bfd3e7e1f04a41d8e1930d978
Jhorman <jhorman.arias.9@gmail.com>
2021-12-01 12:39:27 -0500

    Proyecto página pokemon--NavBar

---
+++ src/components/navigation/NavBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const NavBar = () => {
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <Link to="/">
+                        <img className="navbar-brand " src="./logoPokemon.png" width="100"/>
+                    </Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav mx-auto">
+                            <Link className="nav-link" to="/">Inicio</Link>
+                            <Link className="nav-link" to="/pokemon">Pokemon</Link>
+                            <Link className="nav-link" to="/items">Items</Link>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
---
+++ src/components/pages/Pokemon.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Pokemon = () => {
+    return (
+        <div>
+            <h1> Esta es la pagina Pokemon</h1>
+            <img src="./logoPokemon.png" width="500"/>
+        </div>
+    )
+}
