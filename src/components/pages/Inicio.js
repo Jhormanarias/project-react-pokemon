f7763f6864c832a02bdf5f27acfa5ad48adcdb89
Jhorman <jhorman.arias.9@gmail.com>
2022-01-26 22:56:30 -0500

    Agregado de Carousel de imágenes

--- src/components/pages/Inicio.js
+++ src/components/pages/Inicio.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Carousel from '../carousel/Carousel';
 
 export const Inicio = () => {
     return (
         <div>
+            <Carousel />
             <h1>inicio</h1>
         </div>
     )
