c4a0c7a0adaca639cbbb24d04431b444a42b56d8
Jhorman <jhorman.arias.9@gmail.com>
2022-02-01 18:00:28 -0500

    Consumo de api pokemon con axios, imprimir nombres en pantalla

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,26 +1,65 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+const initialState = {
+  count : 0,
+  pokemon : {
+    pokemons : [],
+    status : "Noloaded",
+    pokeimg : ""
+  }
+};
 
 export const Inicio = () => {
-  const [pokemos, setpokemos] = useState([]);
+  const [pokemos, setpokemos] = useState(initialState.pokemon);
+  const [contador, setContador] = useState(initialState.count);
   useEffect(() => {
-    //peticion
-    let pokemosApi = ["1", "2", "3"];
+    
+    if(pokemos.status=="Noloaded"){
+      //peticion
+    axios.get(`https://pokeapi.co/api/v2/pokemon?limit=50`)
+    .then(({data}) => {
+      console.log(data);
+    //let pokemosApi = data.results;
 
-    setpokemos(pokemosApi);
-  }, []);
+    setpokemos({...pokemos,
+      pokemons:data.results,
+      status: "loaded"}
+    );
+
+  })
+    }
+
+  }, [pokemos]);
 
   const cambiarpokemon = () => {
     let pokemosApi = ["4", "5"];
     setpokemos(pokemosApi);
   };
 
+  const getsrc = (pokemon) =>{
+    axios.get(pokemon)
+    .then(({data}) => {
+      console.log(data.sprites.front_default);
+      const imgSrc = data.sprites.front_default;
+    })
+  };
+
   return (
     <div>
       <h1>
         {" "}
-        Esta es la pagina Pokemon
-        {JSON.stringify(pokemos)}
-        {console.log(pokemos)}
+        Pokemon
+        {pokemos.pokemons.map(pokemon=>{
+          return(
+            <div>
+              <div>
+                {pokemon.name}
+                {console.log(pokemon)}
+                <img src={getsrc(pokemon.url)}></img>
+              </div>
+            </div>
+          )
+        })}
       </h1>
       <Button setpokemos={setpokemos} />
       <Button setpokemos={setpokemos} />
