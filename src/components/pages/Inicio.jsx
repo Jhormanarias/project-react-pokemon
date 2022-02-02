4c1d6264203a29180f98d639febdc436d21aa95f
Jhorman <jhorman.arias.9@gmail.com>
2022-02-01 20:20:15 -0500

    Comentar lineas de código para estudio del mismo, La mayoría del proceso y edición es en la página inicio

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,5 +1,12 @@
+//Importamos Librerias
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+
+//Iniciamos un estado con variable contador(No se usa) 
+//y variable/objeto pokemon donde tiene:
+//pokemons(donde trae todos los pokemon), 
+//status(para saber si ya cargo o no cargo y así ejecutar el useEffect solo con una condición),
+//pokeimg(variable pa probar(pero tampoco se usa))
 const initialState = {
   count : 0,
   pokemon : {
@@ -8,19 +15,23 @@ const initialState = {
     pokeimg : ""
   }
 };
-
+//Para exportar el inicio
 export const Inicio = () => {
-  const [pokemos, setpokemos] = useState(initialState.pokemon);
+  //Aquí asignamos el estado inicial para el useEffect
+  const [pokemos, setpokemos] = useState(initialState.pokemon); 
+  //No se usa
   const [contador, setContador] = useState(initialState.count);
   useEffect(() => {
-    
+    //Solo se va a ejecutar la peticion cuando el estado pokemon aún no haya cargado
     if(pokemos.status=="Noloaded"){
       //peticion
-    axios.get(`https://pokeapi.co/api/v2/pokemon?limit=50`)
+    axios.get(`https://pokeapi.co/api/v2/pokemon?limit=151`)
     .then(({data}) => {
       console.log(data);
     //let pokemosApi = data.results;
 
+    //Asignamos el estado pokemon, 1 los pokemons que trajo de la petición
+    //2 cambiar el status a cargado, para que no ejecute la petición infinitamente
     setpokemos({...pokemos,
       pokemons:data.results,
       status: "loaded"}
@@ -29,12 +40,15 @@ export const Inicio = () => {
   })
     }
 
-  }, [pokemos]);
+  }, [pokemos]); //Aquí pongo a escuchar al useEffect con el estado pokemon
 
+  //Esto es de una prueba anterior
   const cambiarpokemon = () => {
     let pokemosApi = ["4", "5"];
     setpokemos(pokemosApi);
   };
+
+  //Aquí ibamos a usar una función para obtener el link  de la imagen del pokemon
 
   // const getsrc = (pokemon) =>{
   //   axios.get(pokemon)
@@ -44,28 +58,38 @@ export const Inicio = () => {
   //   })
   // };
 
+  //El retorno para lo visual de la página
   return (
     <div>
       <h1>
         {" "}
-        Pokemon
+        {/* Titulo */}
+        Pokemon 
+        {/* Aquí recorremos cada uno de los pokemon que trajo con la funcion map(que sirve para recorrer un objeto)*/}
         {pokemos.pokemons.map(pokemon=>{
           return(
             <div>
               <div>
+                {/* Aquí traemos el nombre del pokemon */}
                 {pokemon.name}
+                {/* Imprimimos en consola lo que trae pokemon */}
                 {console.log(pokemon)}
+                {/* Aquí para la src de la imagen lo traemos de la we pokemondb y para saber que pokemon es le asignamos el nombre que anteriormente traimos */}
                 <img className="pokeImg" src={`https://img.pokemondb.net/artwork/large/${pokemon.name}.jpg`}></img>
               </div>
             </div>
           )
         })}
       </h1>
+
+      {/* Llamo varias veces el componente boton hola*/}
+      <Button setpokemos={setpokemos} />
       <Button setpokemos={setpokemos} />
       <Button setpokemos={setpokemos} />
 
-      <Button setpokemos={setpokemos} />
-
+      {/* En la última imagen del logo hago que cambie el cursor
+      al hacer clic llama la funcion cambiar pokemon(que modifica unos 
+      valores de una prueba anterior con unos diferentes (valor en texto plano)) */}
       <img
         style={{ cursor: "pointer" }}
         onClick={() => cambiarpokemon()}
@@ -75,6 +99,8 @@ export const Inicio = () => {
     </div>
   );
 };
+
+// returnamos boton hola
 const Button = ({ setpokemos }) => {
   function ss(params) {}
   return (
